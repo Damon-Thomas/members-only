3c8aecbc0f3e6342edb46147e4763560dbf9fc47
Damon Thomas <damon22t@gmail.com>
2024-12-13 18:07:03 -0500

    add home and message form styles

--- app.js
+++ app.js
@@ -6,8 +6,8 @@ const appRouter = require("./routes/appRouter");
 
 
 
-// const assetsPath = path.join(__dirname, "public");
-// app.use(express.static(assetsPath));
+const assetsPath = path.join(__dirname, "public");
+app.use(express.static(assetsPath));
 
 app.set("views", path.join(__dirname, "views"));
 app.set("view engine", "ejs");
